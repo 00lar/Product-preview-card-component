1db916f2469d275355b2284fda60e6f001990f39
00lar <eduardani84@gmail.com>
2023-01-31 02:12:36 -0700

    avance mobbile first

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      textColor: {
+        cyan: "hsl(158, 36%, 37%)",
+        grayish: "hsl(228, 12%, 48%)",
+      },
       backgroundColor: {
         cream: "hsl(30, 38%, 92%)",
+        Primary: "hsl(158, 36%, 37%)",
       },
       fontFamily: {
-        monserrat: ["Monserrat", "sans-serif"],
-        monserrat: ["Franceus", "serif"],
+        Monserrat: ["Monserrat", "sans-serif"],
+        Franceus: ["Franceus", "serif"],
+      },
+      backgroundImage: {
+        ProductMobile: "url('/images/product-mobile.jpg')",
+        ProductDesktop: "url('/images/product-desktop.jpg')",
+        CartIcon: "url('/images/icon-cart.svg')",
+      },
     },
   },
   plugins: [],
